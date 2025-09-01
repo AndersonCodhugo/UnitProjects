0e1c6e6662824d028d0f5d7cafa9dd40f2a3b6ef
AndersonCodhugo <anderson_hindo@hotmail.com>
2025-09-01 10:43:17 -0300

    Atividade Grafos em python 01/09/25

---
+++ Grafos/busca_em_profundidade1.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+
+nodes = ['A', 'B', 'C', 'D', 'E']
+
+G.add_nodes_from(nodes)
+
+edges = [('A', 'E'), ('A', 'D'), ('E', 'C'), ('D', 'B')]
+G.add_edges_from(edges)
+
+for node in nodes:
+    start_node = node
+    dfs_nodes = list(nx.dfs_preorder_nodes(G, start_node))
+
+print('Matriz Adjacência')
+matrizA= nx.adjacency_matrix(G)
+print(matrizA.toarray())
+
+print()
+
+print('Matriz Adjacência')
+matrizI= nx.incidence_matrix(G)
+print(matrizI.toarray())
+
+nx.draw(G, with_labels=True)
+plt.show()
