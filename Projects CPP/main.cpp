0f1deb5b441cfe2a196cb8e958df6793c1816733
AndersonCodhugo <anderson_hindo@hotmail.com>
2026-02-11 10:15:42 -0300

    retorno dos estudos de cpp

--- Projects CPP/main.cpp
+++ Projects CPP/main.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
-#include <locale.h>
-
 using namespace std;
 
-
-//comentários são iguais ao java
-int HealthLevel { 0 };
-bool isAlive { HealthLevel > 0 };
-string Name { "Vecna" };
-int Level { 14 };
-
-//testando condicionais e novas formas de definição de variaveis
 int main() {
-    setlocale(LC_ALL, "Portuguese");
-    cout << "Olá mundo!\n";
-    cout << "Meu nome é " << Name;
-    cout << "\n";
-    cout << "Estou nível: " << Level;
-    cout << "\n";
+    int grade[9][9] = {0};
 
-    if (isAlive == true)
-    {
-        cout << "Sim estou vivo.";
-    }else if (isAlive == false)
-    {
-        cout << "Já estou morto.";
+    grade[0][0] = 5;
+    grade[4][4] = 8;
+    grade[8][8] = 9;
+
+    cout << "--- MEU SUDOKU ---" << endl;
+
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            cout << grade[i][j] << " ";
+        }
+        cout << endl;
     }
-//espaço para loopings
+
+    return 0;
 }
